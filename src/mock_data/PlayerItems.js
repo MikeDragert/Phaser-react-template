d973236dcec04f604b8d0ab33c5c8646611b5921
GardRyan <res.gardner@gmail.com>
2024-03-19 17:50:40 -0400

    moved player_items into mock_data

---
+++ src/mock_data/PlayerItems.js
@@ -0,0 +1,95 @@
+
+
+const player_items = {
+    '1': {
+        player_id: '1', 
+        item_id: '1',
+        save_id: '1',
+        container_item_id: '1',
+        location_x: '1',
+        location_y: '1',
+        map_id: 'tutorial'
+    },
+    '2': {
+        player_id: '1', 
+        item_id: '2',
+        save_id: '1',
+        container_item_id: '1',
+        location_x: '1',
+        location_y: '1',
+        map_id: 'tutorial'
+    },
+    '3': {
+        player_id: '1', 
+        item_id: '3',
+        save_id: '1',
+        container_item_id: '1',
+        location_x: '1',
+        location_y: '1',
+        map_id: 'tutorial'
+    },
+    '4': {
+        player_id: '1', 
+        item_id: '4',
+        save_id: '1',
+        container_item_id: '1',
+        location_x: '1',
+        location_y: '1',
+        map_id: 'tutorial'
+    },
+    '5': {
+        player_id: '1', 
+        item_id: '5',
+        save_id: '1',
+        container_item_id: '1',
+        location_x: '1',
+        location_y: '1',
+        map_id: 'tutorial'
+    } , 
+    '6': {
+        player_id: '1', 
+        item_id: '6',
+        save_id: '1',
+        container_item_id: '1',
+        location_x: '1',
+        location_y: '1',
+        map_id: 'tutorial'
+    },
+    '7': {
+        player_id: '1', 
+        item_id: '7',
+        save_id: '1',
+        container_item_id: '1',
+        location_x: '1',
+        location_y: '1',
+        map_id: 'tutorial'
+    },
+    '8': {
+        player_id: '1', 
+        item_id: '8',
+        save_id: '1',
+        container_item_id: '1',
+        location_x: '1',
+        location_y: '1',
+        map_id: 'tutorial'
+    },
+    '9': {
+        player_id: '1', 
+        item_id: '9',
+        save_id: '1',
+        container_item_id: '1',
+        location_x: '1',
+        location_y: '1',
+        map_id: 'tutorial'
+    },
+    '10': {
+        player_id: '1', 
+        item_id: '10',
+        save_id: '1',
+        container_item_id: '1',
+        location_x: '1',
+        location_y: '1',
+        map_id: 'tutorial'
+    }};
+
+    export default player_items;
