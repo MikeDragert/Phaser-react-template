2afcd3cc581debf3068890f2f96a3f5613e057ad
GardRyan <res.gardner@gmail.com>
2024-03-19 17:51:03 -0400

    updated classNames and moved player_items to mock_data

--- src/components/ItemContainer.jsx
+++ src/components/ItemContainer.jsx
@@ -1,114 +1,23 @@
 import React, { useState } from 'react';
-import { DndContext } from '@dnd-kit/core';
+import '../styles/ItemContainer.css'
+import player_items from '../mock_data/PlayerItems';
 
-
-const items = {
-    '1': {
-        player_id: '1', 
-        item_id: '1',
-        save_id: '1',
-        container_item_id: '1',
-        location_x: '1',
-        location_y: '1',
-        map_id: 'tutorial'
-    },
-    '2': {
-        player_id: '1', 
-        item_id: '2',
-        save_id: '1',
-        container_item_id: '1',
-        location_x: '1',
-        location_y: '1',
-        map_id: 'tutorial'
-    },
-    '3': {
-        player_id: '1', 
-        item_id: '3',
-        save_id: '1',
-        container_item_id: '1',
-        location_x: '1',
-        location_y: '1',
-        map_id: 'tutorial'
-    },
-    '4': {
-        player_id: '1', 
-        item_id: '4',
-        save_id: '1',
-        container_item_id: '1',
-        location_x: '1',
-        location_y: '1',
-        map_id: 'tutorial'
-    },
-    '5': {
-        player_id: '1', 
-        item_id: '5',
-        save_id: '1',
-        container_item_id: '1',
-        location_x: '1',
-        location_y: '1',
-        map_id: 'tutorial'
-    } , 
-    '6': {
-        player_id: '1', 
-        item_id: '6',
-        save_id: '1',
-        container_item_id: '1',
-        location_x: '1',
-        location_y: '1',
-        map_id: 'tutorial'
-    },
-    '7': {
-        player_id: '1', 
-        item_id: '7',
-        save_id: '1',
-        container_item_id: '1',
-        location_x: '1',
-        location_y: '1',
-        map_id: 'tutorial'
-    },
-    '8': {
-        player_id: '1', 
-        item_id: '8',
-        save_id: '1',
-        container_item_id: '1',
-        location_x: '1',
-        location_y: '1',
-        map_id: 'tutorial'
-    },
-    '9': {
-        player_id: '1', 
-        item_id: '9',
-        save_id: '1',
-        container_item_id: '1',
-        location_x: '1',
-        location_y: '1',
-        map_id: 'tutorial'
-    },
-    '10': {
-        player_id: '1', 
-        item_id: '10',
-        save_id: '1',
-        container_item_id: '1',
-        location_x: '1',
-        location_y: '1',
-        map_id: 'tutorial'
-    }};
 
     console.log(items);
 
 const ItemContainer = () => { 
     const [itemsState, setItemsState] = useState(items);
     return (
-        <div>
-        <h2>Item Container</h2>
-        <ul>
+        <div className='item-container'>
+            <h2 className='inventory' >Inventory</h2>
             {Object.entries(itemsState).map(([id, item]) => (
-                <li key={id}>{/* Render each item here */}</li>
+                <div key={id} className='grid-item'>
+                    <p>Item ID: {item.item_id}</p>
+                    <p>Player ID: {item.player_id}</p>
+                    {/* Display other item properties here */}
+                </div>
             ))}
-        </ul>
-    </div>
+        </div>
     );
-  };
-
-
+};
 export default ItemContainer;
