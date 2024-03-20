185e080e4f4acb0990646d6177b0fe1d895ceb7a
GardRyan <res.gardner@gmail.com>
2024-03-20 00:35:04 -0400

    removed unnesscary imports and refactored

--- src/components/ItemContainer.jsx
+++ src/components/ItemContainer.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "../styles/ItemContainer.css";
-import DisplayItem from "./components/item";
+import DisplayItem from "./DisplayItem";
+
 
 const ItemContainer = ({ items }) => {
     return (
         <div className="item-container">
             <h2 className="inventory">Inventory</h2>
-            <DisplayItem />
+            <DisplayItem items={items} />
         </div>
     );
 };
