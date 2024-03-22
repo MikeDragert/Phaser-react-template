d9d0cd58961882be71a51822624d3d5b8d986fd8
GardRyan <res.gardner@gmail.com>
2024-03-22 10:43:50 -0400

    added achievements and highscores

---
+++ src/components/Achievements.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+//import "./Achievements.css";
+
+const Achievements = () => {
+    return (
+        <div className="achievements">
+            <h1>Achievements</h1>
+            <ol>
+                <li>First Blood</li>
+                <li>Double Kill</li>
+                <li>Triple Kill</li>
+            </ol>
+        </div>
+    );
+};
+
+export default Achievements;
---
+++ src/components/Highscores.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+//import "./Highscores.css";
+
+const Highscores = () => {
+    return (
+        <div className="highscores">
+            <h1>Highscores</h1>
+            <ol>
+                <li>Player 1: 1000</li>
+                <li>Player 2: 900</li>
+                <li>Player 3: 800</li>
+            </ol>
+        </div>
+    );
+};
+
+export default Highscores;
