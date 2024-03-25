1fb65ad72ddcff48670ab4cf44d5d8b231163131
GardRyan <res.gardner@gmail.com>
2024-03-24 21:45:25 -0400

    added isLoggedIn Logic to show login/register or logout

--- src/components/TopNavigationBar.jsx
+++ src/components/TopNavigationBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 //import "./styles/TopNavigationBar.css";
 
-const TopNavigationBar = () => {
+const TopNavigationBar = ({ isLoggedIn }) => {
     return (
         <nav className="top-navigation-bar">
             <span className="site-title">Welcome to Labber!</span>
@@ -18,16 +18,22 @@ const TopNavigationBar = () => {
                     </li>
                 </ul>
             </div>
-            <div className="login-regsiter-links">
-                <ul>
-                    <li>
-                        <a href="/login">Login</a>
-                    </li>
-                    <li>
-                        <a href="/register">Register</a>
-                    </li>
-                </ul>
-            </div>
+            {!isLoggedIn ? (
+                <div className="login-regsiter-links">
+                    <ul>
+                        <li>
+                            <a href="/login">Login</a>
+                        </li>
+                        <li>
+                            <a href="/register">Register</a>
+                        </li>
+                    </ul>
+                </div>
+            ) : (
+                <div className="logout-link">
+                    <a href="/logout">Logout</a>
+                </div>
+            )}
         </nav>
     );
 };
