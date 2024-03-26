32e599cb179bdd7a654063b978dc3ad34a2c1d0d
Jonas-Kunz <jonas_K_7@outlook.com>
2024-03-26 14:22:37 -0300

    Resolve Text bug AGAIN

--- src/game/scenes/MainMenu.js
+++ src/game/scenes/MainMenu.js
@@ -14,7 +14,7 @@ export class MainMenu extends Scene {
     this.logo = this.add.image(512, 300, "logo").setDepth(100);
 
     this.add
-      .text(512, 460, "Main Menu", {
+      .text(512, 460, "Labber", {
         fontFamily: "Arial Black",
         fontSize: 38,
         color: "#ffffff",
